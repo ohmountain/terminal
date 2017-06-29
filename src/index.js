4106cb788d832ef5bf108423ccbe3940fc045376
renshan <1005110700@qq.com>
2017-06-29 15:59:50 +0800

    f

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import "./styles/css/normalize.css"
 
 import registerServiceWorker from './registerServiceWorker';
 
+var music = 'http://nhds.oss-cn-hangzhou.aliyuncs.com/picture-wall/audio/kaimushi/test_94448214.mp3';
+
+window.addEventListener('load', () => {
+    var au = document.createElement('audio');
+    au.src = music;
+    au.volume = .2;
+    document.body.appendChild(au);
+    au.play();
+    au.addEventListener('ended',  () => au.remove());
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
